afc1d0b2b8503262e2c31d47c3b17c38bc084cc7
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-07 22:34:35 +0300

    Add Task4

---
+++ Task4Dop/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество кустов: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int sum = 0;
+
+while (n < 3 || n > 1000)
+
+{
+    Console.WriteLine("Вы ошиблись! \nВведите правильное кол-во кустов: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int[] a = new int[n];               // количество кустов
+n = a.Length;
+for(int i = 0;i < a.Length; i ++)
+{
+    a[i]= new Random().Next(5,15);  // все кусты с разным количеством ягод
+    Console.WriteLine(" " + i + " = " + a[i]);         // вывел для сверки
+}
+
+Console.WriteLine("Введите номер куста: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+    sum = a[x]+(a[x-1])+a[x+1];
+
+Console.Write($"Количестово ягод которые соберет модуль за один заход:   =  {sum}  " );
