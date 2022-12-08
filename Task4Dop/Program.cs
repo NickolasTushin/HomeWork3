95b7094695d737b56b741c58bbcf6d3930e2ae57
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-08 14:31:11 +0300

    UPD Task4

--- Task4Dop/Program.cs
+++ Task4Dop/Program.cs
@@ -1,10 +1,8 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите количество кустов: ");
+Console.WriteLine("Введите количество кустов не меньше трех:  ");
 int n = Convert.ToInt32(Console.ReadLine()); 
 int sum = 0;
-
 while (n < 3 || n > 1000)
-
 {
     Console.WriteLine("Вы ошиблись! \nВведите правильное кол-во кустов: ");
     n = Convert.ToInt32(Console.ReadLine());
@@ -13,13 +11,25 @@ int[] a = new int[n];               // количество кустов
 n = a.Length;
 for(int i = 0;i < a.Length; i ++)
 {
-    a[i]= new Random().Next(5,15);  // все кусты с разным количеством ягод
-    Console.WriteLine(" " + i + " = " + a[i]);         // вывел для сверки
+    a[i]= new Random().Next(5,20);  // все кусты с разным количеством ягод
+    Console.WriteLine("Номер куста " + i + "     Количество ягод на нем     " + a[i]);        // вывел для сверки
 }
-
 Console.WriteLine("Введите номер куста: ");
 int x = Convert.ToInt32(Console.ReadLine());
+if (x < 0 || x >= a.Length) 
+{
+    x = 0;
+}
+int prev = x - 1;
+if (prev <= 0) 
+{
+  prev = a.Length -1;
+}
+int next = x + 1;
+if (next >= a.Length) 
+{
+  next = 0;
+}
+sum = a[x] + a[prev] + a[next];
 
-    sum = a[x]+(a[x-1])+a[x+1];
-
-Console.Write($"Количестово ягод которые соберет модуль за один заход:   =  {sum}  " );
+    Console.Write($"Количестово ягод которые соберет модуль за один заход:   =  {sum}  " );
